3b8d7ae5f52485819fb95f7fd4c4db391c8dbd70
Dawaman43 <dawitworkujima@gmail.com>
2025-12-10 09:05:59 +0300

    implement factorial calculation with user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
-}
+    use std::io;
+
+    fn main() {
+        println!("Factorial sequence:");
+        println!("Enter a number to calculate its factorial value:");
+        let mut input = String::new();
+        io::stdin().
+            read_line(&mut input)
+            .expect("Failed to read line");
+        
+        let input: u128 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                return;
+            }
+        };
+
+        let mut mul: u128 = 1;
+        
+        for n in 0..input {
+            mul *= input - n;
+        }
+
+        println!("mul is {mul}")
+    }
